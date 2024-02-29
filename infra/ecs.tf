7d451239f0c93b90bee294131f6d41c68f739d03
yamadatt <yamadatt@gmail.com>
2024-02-29 22:43:49 +0900

    add

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -30,12 +30,12 @@ resource "aws_ecs_task_definition" "main" {
           appProtocol   = "http"
         }
       ]
-      mountPoints = [
-        {
-          containerPath = "/usr/share/nginx/html"
-          sourceVolume  = "fargate-efs"
-        }
-      ]
+      # mountPoints = [
+      #   {
+      #     containerPath = "/usr/share/nginx/html/"
+      #     sourceVolume  = "fargate-efs"
+      #   }
+      # ]
       essential = true
       logConfiguration = {
         logDriver : "awslogs"
@@ -52,13 +52,13 @@ resource "aws_ecs_task_definition" "main" {
     cpu_architecture        = "X86_64"
     operating_system_family = "LINUX"
   }
-  volume {
-    name = "fargate-efs"
-    efs_volume_configuration {
-      file_system_id = aws_efs_file_system.efs.id
-      root_directory = "/"
-    }
-  }
+  # volume {
+  #   name = "fargate-efs"
+  #   efs_volume_configuration {
+  #     file_system_id = aws_efs_file_system.efs.id
+  #     root_directory = "/"
+  #   }
+  # }
 
 }
   # container_definitions    = <<-EOS
@@ -99,7 +99,7 @@ resource "aws_ecs_service" "main" {
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.main.arn
   launch_type     = "FARGATE"
-  enable_execute_command = true #ECS EXECの有効
+  # enable_execute_command = true #ECS EXECの有効
   desired_count = 1
 
 
